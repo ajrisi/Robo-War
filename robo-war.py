2d18d0594eb905b09d9d0cbc90a0fbeb6262bd17
Jonathan Potter <jonathanpotter@jonathan-potters-macbook-pro.local>
2009-10-08 11:13:09 -0400

    Added a small bit of curses, and other things.

--- robo-war.py
+++ robo-war.py
@@ -10,27 +10,50 @@ Genetic Algorithms, 20091
 import sys
 import os
 import random
+import curses
 
-maxInstructions = 5
 
-
-def runGA():
+def runGA(env):
     populationLimit = 6
     maxTime = 10000
-
+	maxInstructions = 5
+	possibleInstructions = ["forward", 
+							"reverse", 
+							"spin_left", 
+							"spin_right",
+							"fire"]
+							
     # create the initial population
-    population = initPopulation(populationLimit)
+    population = initPopulation(possibleInsructions, maxInstructions, populationLimit)
 
     # when one dies, crossover/mutate, add new to pop, remove old
 
-    for timeSlice in range(1, maxTime): 
+    for timeSlice in range(1, maxTime):
         # run robots
         # for each killed, crossover/mutate, add new, remove old
+		for robot in population:
+			runRobot(env, robot)
+
+
+def initPopulation(possibleInsructions, maxInstructions, populationLimit):
+	population = []
+	
+	for i in range(populationLimit):
+		robot = {}
+		robot["x"] = 0
+		robot["y"] = 0
+		robot["direction"] = "up"
+		robot["instructions"] = [random.choice(possibleInsructions) for i in range(maxInstructions)]
+		
+		population += robot
+	
+	return population
 
 
 def runRobot(env, robot):
     for instruction in robot["program"]:
         executeInstruction(env, robot, instruction)
+
 
 def executeInstruction(env, robot, instruction):
     if instruction == "forward":
@@ -70,5 +93,13 @@ def executeInstruction(env, robot, instruction):
         elif robot["direction"] == "right":
             robot["direction"] = "down"
 
-            
 
+def drawRobot(env, robot):
+	env.addstr(robot["x"], robot["y"], "#")
+	env.refresh()
+
+def main():
+	curses.wrapper(runGA)
+
+if __name__ == '__main__':
+	main()

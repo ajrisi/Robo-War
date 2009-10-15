83780780db96d4eee27fb75e1a21a4b209206b88
Jonathan Potter <jonathanpotter@juggernaut.wireless.rit.edu>
2009-10-15 16:32:32 -0400

    Added class for Bullet
    Revamped all classes to add robustness and to prevent passing env and arena everywhere
    
    Bullet animations
    
    Bullet wall and robot collisions
    
    Hit robot health reductions

--- robo-war.py
+++ robo-war.py
@@ -23,13 +23,107 @@ class Point:
 		self.y = y
 
 
+class Bullet:
+	# static
+	staticIdCounter = 0
+	
+	# instance
+	bulletId = None
+	location = None
+	direction = None
+	damage = None
+	arena = None
+	
+	def __init__(self, location = Point(), direction = "up", damage = 1):
+		self.bulletId = Bullet.staticIdCounter
+		Bullet.staticIdCounter += 1
+		
+		self.location = location
+		self.direction = direction
+		self.damage = damage
+	
+	
+	def draw(self):
+		self.arena.env.addstr(self.location.y, self.location.x, ".")
+		self.arena.env.refresh()
+	
+	
+	def go(self):
+		while not self.advance(): time.sleep(.05)
+	
+	
+	def advance(self):
+		# see if we are already colliding with something
+		hadCollision = self.doCollision()
+		
+		oldX = self.location.x
+		oldY = self.location.y
+		
+		self.location = nextPosition(self.location, self.direction)
+		
+		# see if we will collide when we advance
+		hadCollision = (self.doCollision() or hadCollision)
+		
+		self.arena.env.addstr(oldY, oldX, " ")
+		self.arena.env.refresh()
+		
+		if not hadCollision:
+			self.draw()
+		else: # had collision
+			self.arena.env.addstr(oldY, oldX, "*")
+			self.arena.env.refresh()
+		
+		return hadCollision
+	
+	
+	def doCollision(self):
+		# check for wall hit
+		if self.location.x > self.arena.dimensions.x:
+			return True
+		elif self.location.y > self.arena.dimensions.y:
+			return True
+		elif self.location.x < 0:
+			return True
+		elif self.location.y < 0:
+			return True
+		
+		# check for robot hit
+		for r in self.arena.robots:
+			if r.location.x == self.location.x and r.location.y == self.location.y:
+				# we hit robot r
+				r.health -= self.damage
+				
+				self.arena.env.addstr(0, 0, str(r.health))
+				return True
+		
+		return False
+
+
 class Arena:
 	robots = None
+	bullets = None
 	dimensions = None
+	env = None
 	
-	def __init__(self, xDimension, yDimension):
+	def __init__(self, xDimension, yDimension, env = None):
 		self.robots = []
+		self.bullets = []
 		self.dimensions = Point(xDimension, yDimension)
+		self.env = env
+	
+	
+	def fire(self, bullet):
+		self.addBullets([bullet])
+		bullet.go()
+	
+	
+	def addBullets(self, bullets):
+		for b in bullets:
+			# don't add duplicates
+			if self.indexOfBullet(b) < 0:
+				b.arena = self
+				self.bullets += [b]
+	
 	
 	def addRobots(self, robots):
 		for r in robots:
@@ -38,9 +132,14 @@ class Arena:
 				r.arena = self
 				self.robots += [r]
 	
-	def fire(self, location, direction, env):
-		pass
-			
+	def indexOfBullet(self, bullet):
+		for i in range(len(self.bullets)):
+			if self.bullets[i].bulletId == bullet.bulletId:
+				return i
+		
+		return -1
+	
+	
 	def indexOfRobot(self, robot):
 		for i in range(len(self.robots)):
 			if self.robots[i].robotId == robot.robotId:
@@ -58,7 +157,6 @@ class Arena:
 				return 1
 		
 		return 0
-
 
 
 class Robot:
@@ -83,23 +181,26 @@ class Robot:
 		self.health = 100
 	
 	
-	def draw(self, env):
-		env.addstr(self.location.y, self.location.x, self.displayString())
-		env.refresh()
+	def draw(self):
+		self.arena.env.addstr(self.location.y, self.location.x, self.displayString())
+		self.arena.env.refresh()
 	
-	def runInstructions(self, env):
+	
+	def runInstructions(self):
 		for x in self.instructions:
-			self.runInstruction(x, env)
+			self.runInstruction(x)
 	
-	def runInstruction(self, x, env):
+	
+	def runInstruction(self, x):
 		action = {  "forward":    self.moveForward, 
 					"reverse":    self.moveReverse, 
 					"spin_left":  self.spinLeft, 
 					"spin_right": self.spinRight,
 					"fire":       self.fire }
 		
-		action[x](env)
+		action[x]()
 		time.sleep(.5)
+	
 	
 	def displayString(self):
 		if self.direction == "up":
@@ -112,42 +213,46 @@ class Robot:
 			display = ">"
 		
 		return display
-
-
-	def fire(self, env):
-		self.arena.fire(self.location, self.direction, env)
+		
+		
+	def fire(self):
+		bulletDirection = self.direction
+		bulletLocation = nextPosition(self.location, bulletDirection)
+		
+		bullet = Bullet(bulletLocation, bulletDirection)
+		self.arena.fire(bullet)
 	
-
-	def spinLeft(self, env):
-		self.spin(-1, env)
-
-
-	def spinRight(self, env):
-		self.spin(1, env)
-
-
+		
+	def spinLeft(self):
+		self.spin(-1)
+		
+		
+	def spinRight(self):
+		self.spin(1)
+		
+		
 	# direction should be 1/-1 for right/left respectively
-	def spin(self, direction, env):
+	def spin(self, direction):
 		d = ["up", "right", "down", "left"]
 		currentIndex = d.index(self.direction)
 		newIndex = (currentIndex + (1*direction)) % len(d)
 		self.direction = d[newIndex]
 		
-		env.addstr(self.location.y, self.location.x, " ")
-		env.addstr(self.location.y, self.location.x, self.displayString())
-		env.refresh()
-
-
-	def moveForward(self, env):
-		self.move(1, env)
-
-
-	def moveReverse(self, env):
-		self.move(-1, env)
-	
-	
+		self.arena.env.addstr(self.location.y, self.location.x, " ")
+		self.arena.env.addstr(self.location.y, self.location.x, self.displayString())
+		self.arena.env.refresh()
+		
+		
+	def moveForward(self):
+		self.move(1)
+		
+		
+	def moveReverse(self):
+		self.move(-1)
+		
+		
 	# direction should be -1/1 for reverse/forward respectively
-	def move(self, direction, env):
+	def move(self, direction):
 		deltaX = 0
 		deltaY = 0
 		
@@ -165,10 +270,10 @@ class Robot:
 		
 		# if no collision (legal move)
 		if 0 == self.arena.checkCollision(Point(newX, newY), self):			
-			env.addstr(self.location.y, self.location.x, " ")
-			env.addstr(newY, newX, self.displayString())
-			env.refresh()
-	
+			self.arena.env.addstr(self.location.y, self.location.x, " ")
+			self.arena.env.addstr(newY, newX, self.displayString())
+			self.arena.env.refresh()
+		
 			self.location.x = newX
 			self.location.y = newY
 
@@ -179,24 +284,24 @@ def runGA(env):
 	maxTime = 10
 	maxInstructions = 5
 	possibleInstructions = ["forward", "reverse", "spin_left", "spin_right", "fire"]
-							
+	
 	# create the initial population
 	population = initPopulation(possibleInstructions, maxInstructions, populationLimit)
 	
 	# add robots to the arena
-	arena = Arena(100, 100)
+	arena = Arena(50, 20, env)
 	arena.addRobots(population)
 	
 	# when one dies, crossover/mutate, add new to pop, remove old
 	
 	# draw all of our robots to start with
 	for robot in arena.robots:
-		robot.draw(env)
+		robot.draw()
 	
 	for timeSlice in range(1, maxTime):
 		# run robots
 		for robot in arena.robots:
-			robot.runInstructions(env)
+			robot.runInstructions()
 		
 		# for each killed, crossover/mutate, add new, remove old
 		
@@ -206,14 +311,14 @@ def initPopulation(possibleInsructions, maxInstructions, populationLimit):
 	population = []
 	
 	r1 = Robot()
-	r1.instructions = ["forward", "forward", "forward", "forward", "spin_right"]
+	r1.instructions = ["forward", "forward", "forward", "fire", "spin_right"]
 	r1.location.x = 10
 	r1.location.y = 10
 	
 	r2 = Robot()
-	r2.instructions = ["forward", "forward", "forward", "forward", "spin_left"]
+	r2.instructions = ["forward", "forward", "forward", "fire", "spin_left"]
 	r2.location.x = 20
-	r2.location.y = 20
+	r2.location.y = 10
 	
 	population = [r1, r2]
 	
@@ -226,9 +331,27 @@ def initPopulation(possibleInsructions, maxInstructions, populationLimit):
 	return population
 
 
+def nextPosition(location, direction, amount = 1):
+	newLocation = Point(location.x, location.y)
+	
+	if direction == "up" or direction == "down":
+		if direction == "up": delta = -1 * amount
+		else:                 delta = amount
+		
+		newLocation.y += delta
+	
+	
+	elif direction == "left" or direction == "right":
+		if direction == "left": delta = -1 * amount
+		else:                   delta = amount
+		
+		newLocation.x += delta
+	
+	return newLocation
+
 
 def main():
 	curses.wrapper(runGA)
-
+	
 if __name__ == '__main__':
 	main()
